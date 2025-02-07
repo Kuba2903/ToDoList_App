d3773518611e32cd294b1c8bc804cd537d0dd66c
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2025-02-07 17:37:00 +0100

    add TODO model

---
+++ TODOAPP/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODOAPP.Models
+{
+    public class ToDoItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Importance { get; set; } = string.Empty;
+    }
+}
