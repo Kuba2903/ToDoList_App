d6e54bd1d10baab1798c7050a4e6c86ef237ceaa
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2025-02-08 14:30:56 +0100

    change the structure to MVVM pattern

---
+++ TODOAPP/ViewModels/ToDoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TODOAPP.Models;
+
+namespace TODOAPP.ViewModels
+{
+    public partial class ToDoViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<ToDoItem> _todoList = new();
+
+        public void AddTodo(ToDoItem todo)
+        {
+            if (!string.IsNullOrWhiteSpace(todo.Title))
+            {
+                _todoList.Add(todo);
+            }
+        }
+    }
+}
---
+++ TODOAPP/Views/Popups/NewTodoPopup.xaml.cs
@@ -0,0 +1,36 @@
+using CommunityToolkit.Maui.Views;
+using TODOAPP.Models;
+using TODOAPP.ViewModels;
+
+namespace TODOAPP;
+
+public partial class NewTodoPopup : Popup
+{
+    private readonly ToDoViewModel _viewModel;
+
+    public NewTodoPopup(ToDoViewModel viewModel)
+	{
+		InitializeComponent();
+        _viewModel = viewModel;
+	}
+
+    private void SaveTODOClicked(object sender, EventArgs e)
+    {
+        string importance = HighRadioButton.IsChecked ? "High" :
+                            MediumRadioButton.IsChecked ? "Medium" : "Low";
+
+        var newTodo = new ToDoItem
+        {
+            Title = TaskTitle.Text,
+            Description = TaskDescription.Text,
+            Importance = importance
+        };
+
+        _viewModel.AddTodo(newTodo);
+        Close();
+    }
+    private void CancelTODOClicked(object sender, EventArgs e)
+    {
+        Close();
+    }
+}
